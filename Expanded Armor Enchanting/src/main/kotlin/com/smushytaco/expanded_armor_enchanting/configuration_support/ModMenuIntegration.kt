c003e61223e79b44bf816a8475526a1764796bb9
SmushyTaco <smushytaco@gmail.com>
2021-05-25 12:43:02 -0700

    Initial Commit

---
+++ Expanded Armor Enchanting/src/main/kotlin/com/smushytaco/expanded_armor_enchanting/configuration_support/ModMenuIntegration.kt
@@ -0,0 +1,14 @@
+package com.smushytaco.expanded_armor_enchanting.configuration_support
+import com.smushytaco.expanded_armor_enchanting.ExpandedArmorEnchanting
+import io.github.prospector.modmenu.api.ConfigScreenFactory
+import io.github.prospector.modmenu.api.ModMenuApi
+import me.shedaniel.autoconfig.AutoConfig
+import net.fabricmc.api.EnvType
+import net.fabricmc.api.Environment
+@Environment(EnvType.CLIENT)
+class ModMenuIntegration: ModMenuApi {
+    override fun getModId() = ExpandedArmorEnchanting.MOD_ID
+    override fun getModConfigScreenFactory(): ConfigScreenFactory<*> {
+        return ConfigScreenFactory { parent -> AutoConfig.getConfigScreen(ModConfiguration::class.java, parent).get() }
+    }
+}
