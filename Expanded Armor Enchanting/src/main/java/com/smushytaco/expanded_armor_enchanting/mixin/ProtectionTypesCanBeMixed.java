54b730b0aa621bfa0111fce9a19dcaf99facb6f4
Nikan Radan <github.com@alias.nikanradan.com>
2023-06-15 05:26:01 -0700

    1.20.1

--- Expanded Armor Enchanting/src/main/java/com/smushytaco/expanded_armor_enchanting/mixin/ProtectionTypesCanBeMixed.java
+++ Expanded Armor Enchanting/src/main/java/com/smushytaco/expanded_armor_enchanting/mixin/ProtectionTypesCanBeMixed.java
@@ -1,4 +1,5 @@
 package com.smushytaco.expanded_armor_enchanting.mixin;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.smushytaco.expanded_armor_enchanting.ExpandedArmorEnchanting;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.ProtectionEnchantment;
@@ -6,16 +7,12 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(ProtectionEnchantment.class)
 public abstract class ProtectionTypesCanBeMixed {
     @Shadow
     @Final
     public ProtectionEnchantment.Type protectionType;
-    @Inject(method = "canAccept", at = @At("HEAD"), cancellable = true)
-    private void hookCanAccept(Enchantment other, CallbackInfoReturnable<Boolean> cir) {
-        if (!ExpandedArmorEnchanting.INSTANCE.getConfig().getCanMixDifferentProtectionTypes() || !(other instanceof ProtectionEnchantment protectionEnchantment)) return;
-        if (protectionType != protectionEnchantment.protectionType) cir.setReturnValue(true);
-    }
+    @ModifyReturnValue(method = "canAccept", at = @At("RETURN"))
+    @SuppressWarnings("unused")
+    private boolean hookCanAccept(boolean original, Enchantment other) { return !ExpandedArmorEnchanting.INSTANCE.getConfig().getCanMixDifferentProtectionTypes() || !(other instanceof ProtectionEnchantment protectionEnchantment) ? original : protectionType != protectionEnchantment.protectionType || original; }
 }
