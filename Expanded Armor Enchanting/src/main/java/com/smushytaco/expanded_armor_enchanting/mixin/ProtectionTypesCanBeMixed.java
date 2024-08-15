9c57285962840b6b9c47967501688d0c53dd68b5
Nikan Radan <github.com@alias.nikanradan.com>
2024-08-15 15:31:51 -0700

    1.21.1

--- Expanded Armor Enchanting/src/main/java/com/smushytaco/expanded_armor_enchanting/mixin/ProtectionTypesCanBeMixed.java
+++ Expanded Armor Enchanting/src/main/java/com/smushytaco/expanded_armor_enchanting/mixin/ProtectionTypesCanBeMixed.java
@@ -6,28 +6,12 @@ import net.minecraft.enchantment.Enchantments;
 import net.minecraft.registry.entry.RegistryEntry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import java.util.Set;
 @Mixin(Enchantment.class)
 public abstract class ProtectionTypesCanBeMixed {
     @ModifyReturnValue(method = "canBeCombined", at = @At("RETURN"))
     private static boolean hookCanBeCombined(boolean original, RegistryEntry<Enchantment> first, RegistryEntry<Enchantment> second) {
         if (!ExpandedArmorEnchanting.INSTANCE.getConfig().getCanMixDifferentProtectionTypes()) return original;
-        if (first.matchesKey(Enchantments.PROTECTION)) {
-            if (second.matchesKey(Enchantments.PROJECTILE_PROTECTION) || second.matchesKey(Enchantments.BLAST_PROTECTION) || second.matchesKey(Enchantments.FIRE_PROTECTION)) return true;
-        } else if (first.matchesKey(Enchantments.PROJECTILE_PROTECTION)) {
-            if (second.matchesKey(Enchantments.PROTECTION) || second.matchesKey(Enchantments.BLAST_PROTECTION) || second.matchesKey(Enchantments.FIRE_PROTECTION)) return true;
-        } else if (first.matchesKey(Enchantments.BLAST_PROTECTION)) {
-            if (second.matchesKey(Enchantments.PROTECTION) || second.matchesKey(Enchantments.PROJECTILE_PROTECTION) || second.matchesKey(Enchantments.FIRE_PROTECTION)) return true;
-        } else if (first.matchesKey(Enchantments.FIRE_PROTECTION)) {
-            if (second.matchesKey(Enchantments.PROTECTION) || second.matchesKey(Enchantments.PROJECTILE_PROTECTION) || second.matchesKey(Enchantments.BLAST_PROTECTION)) return true;
-        } else if (second.matchesKey(Enchantments.PROTECTION)) {
-            if (first.matchesKey(Enchantments.PROJECTILE_PROTECTION) || first.matchesKey(Enchantments.BLAST_PROTECTION) || first.matchesKey(Enchantments.FIRE_PROTECTION)) return true;
-        } else if (second.matchesKey(Enchantments.PROJECTILE_PROTECTION)) {
-            if (first.matchesKey(Enchantments.PROTECTION) || first.matchesKey(Enchantments.BLAST_PROTECTION) || first.matchesKey(Enchantments.FIRE_PROTECTION)) return true;
-        } else if (second.matchesKey(Enchantments.BLAST_PROTECTION)) {
-            if (first.matchesKey(Enchantments.PROTECTION) || first.matchesKey(Enchantments.PROJECTILE_PROTECTION) || first.matchesKey(Enchantments.FIRE_PROTECTION)) return true;
-        } else if (second.matchesKey(Enchantments.FIRE_PROTECTION)) {
-            if (first.matchesKey(Enchantments.PROTECTION) || first.matchesKey(Enchantments.PROJECTILE_PROTECTION) || first.matchesKey(Enchantments.BLAST_PROTECTION)) return true;
-        }
-        return original;
+        return original || ExpandedArmorEnchanting.INSTANCE.canCombineEnchantments(first, second, Set.of(Enchantments.PROTECTION, Enchantments.PROJECTILE_PROTECTION, Enchantments.BLAST_PROTECTION, Enchantments.FIRE_PROTECTION));
     }
 }
--- Expanded Armor Enchanting/src/main/kotlin/com/smushytaco/expanded_armor_enchanting/ExpandedArmorEnchanting.kt
+++ Expanded Armor Enchanting/src/main/kotlin/com/smushytaco/expanded_armor_enchanting/ExpandedArmorEnchanting.kt
@@ -4,6 +4,10 @@ import me.shedaniel.autoconfig.AutoConfig
 import me.shedaniel.autoconfig.annotation.Config
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer
 import net.fabricmc.api.ModInitializer
+import net.minecraft.enchantment.Enchantment
+import net.minecraft.registry.RegistryKey
+import net.minecraft.registry.entry.RegistryEntry
+import kotlin.jvm.optionals.getOrNull
 object ExpandedArmorEnchanting : ModInitializer {
     const val MOD_ID = "expanded_armor_enchanting"
     lateinit var config: ModConfiguration
@@ -14,4 +18,5 @@ object ExpandedArmorEnchanting : ModInitializer {
         }
         config = AutoConfig.getConfigHolder(ModConfiguration::class.java).config
     }
+    fun canCombineEnchantments(first: RegistryEntry<Enchantment>, second: RegistryEntry<Enchantment>, enchantments: Set<RegistryKey<Enchantment>>) = enchantments.any { first.matchesKey(it) } && enchantments.any { second.matchesKey(it) } && !first.matchesKey(second.key.getOrNull())
 }
